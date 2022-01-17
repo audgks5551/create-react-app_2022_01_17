b7272fbc9bc93b1a718c03db87b7b8aa0becbe83
audgks5551 <audgks5551@daum.net>
2022-01-17 19:11:05 +0900

    useEffect 사용해보기

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import { useState, useEffect } from "react";
+
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => {
+    setCounter((prev) => prev + 1);
+  }
+  const onChange = (event) => {
+    setKeyword(event.target.value)
+  }
+
+  useEffect(() => {
+    console.log("한번만 실행됨", keyword);
+  }, []);
+  useEffect(() => {
+    console.log("클릭", counter);
+  }, [counter]);
+  useEffect(() => {
+    if (keyword !== "" && keyword.length > 5) {
+      console.log("검색", keyword);
+    }
+  }, [keyword]);
+
   return (
     <div>
-      <h1 className={styles.title}>드디어 시작이다</h1>
-      <Button text="save" />
+      <div>
+        <input value={keyword} onChange={onChange} type="text" />
+      </div>
+      <div>
+        <span>{counter}</span>
+      </div>
+      <div>
+        <button onClick={onClick}>클릭</button>
+      </div>
     </div>
   );
 }
